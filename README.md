6fc023b2ce472c1e9b91b294e8578c9cbc5bb434
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-19 08:25:38 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sharpfuzz
+# SharpFuzz
---
+++ src/SharpFuzz.Common/Trace.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SharpFuzz.Common
+{
+	public static class Trace
+	{
+		public static readonly byte[] SharedMem = new byte[65536];
+		public static int PrevLocation = 0;
+	}
+}
