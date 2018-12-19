32684eef7a0d640889b1572678e4583d69f3fd4d
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-19 19:30:09 +0100

    Add BouncyCastle sample

---
+++ src/SharpFuzz.Samples/BouncyCastle/BouncyCastle.Fuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Org.BouncyCastle.Asn1;
+using SharpFuzz;
+
+namespace BouncyCastle.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenRead(args[0]))
+					using (var asn = new Asn1InputStream(file))
+					{
+						while (asn.ReadObject() != null) { }
+					}
+				}
+				catch (ArgumentException) { }
+				catch (Asn1Exception) { }
+				catch (Asn1ParsingException) { }
+				catch (InvalidCastException) { }
+				catch (InvalidOperationException) { }
+				catch (IOException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/BouncyCastle/BouncyCastle.Run/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Org.BouncyCastle.Asn1;
+
+namespace BouncyCastle.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenRead(args[0]))
+			using (var asn = new Asn1InputStream(file))
+			{
+				while (asn.ReadObject() != null) { }
+			}
+		}
+	}
+}
