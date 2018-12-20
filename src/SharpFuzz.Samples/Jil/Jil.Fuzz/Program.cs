7c6d3c9effa79296987d23282e8b78d11a428e58
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-20 12:29:25 +0100

    Add Jil

---
+++ src/SharpFuzz.Samples/Jil/Jil.Fuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using SharpFuzz;
+
+namespace Jil.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenText(args[0]))
+					{
+						JSON.DeserializeDynamic(file);
+					}
+				}
+				catch (ArgumentException) { }
+				catch (DeserializationException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/Jil/Jil.Run/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Jil.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenText(args[0]))
+			{
+				JSON.DeserializeDynamic(file);
+			}
+		}
+	}
+}
