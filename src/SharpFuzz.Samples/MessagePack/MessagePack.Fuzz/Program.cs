90310f77220e91caf1643767fd2459ccf50ed43a
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-20 19:46:47 +0100

    Add MessagePack

---
+++ src/SharpFuzz.Samples/MessagePack/MessagePack.Fuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using MessagePack.Resolvers;
+using SharpFuzz;
+
+namespace MessagePack.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenRead(args[0]))
+					{
+						MessagePackSerializer.Deserialize<dynamic>(file, ContractlessStandardResolver.Instance);
+					}
+				}
+				catch (ArgumentException) { }
+				catch (IndexOutOfRangeException) { }
+				catch (InvalidOperationException) { }
+				catch (OverflowException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/MessagePack/MessagePack.Run/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using MessagePack.Resolvers;
+
+namespace MessagePack.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenRead(args[0]))
+			{
+				MessagePackSerializer.Deserialize<dynamic>(file, ContractlessStandardResolver.Instance);
+			}
+		}
+	}
+}
