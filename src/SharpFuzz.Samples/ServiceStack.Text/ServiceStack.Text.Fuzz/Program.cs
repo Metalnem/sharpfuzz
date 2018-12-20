873f434076f7bf455f2d2f392e1ad790618ef3e9
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-20 18:59:04 +0100

    Add ServiceStack.Text

---
+++ src/SharpFuzz.Samples/ServiceStack.Text/ServiceStack.Text.Fuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using SharpFuzz;
+
+namespace ServiceStack.Text.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					var text = File.ReadAllText(args[0]);
+					JsonObject.Parse(text);
+				}
+				catch (ArgumentOutOfRangeException) { }
+				catch (IndexOutOfRangeException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/ServiceStack.Text/ServiceStack.Text.Run/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace ServiceStack.Text.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			JsonObject.Parse(text);
+		}
+	}
+}
