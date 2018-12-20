1c45a8291ba31c1a70005336049dc55fdf968928
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-20 14:02:19 +0100

    Add Newtonsoft.Json

---
+++ src/SharpFuzz.Samples/Newtonsoft.Json/Newtonsoft.Json.Fuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using SharpFuzz;
+
+namespace Newtonsoft.Json.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					var text = File.ReadAllText(args[0]);
+					JsonConvert.DeserializeObject(text);
+				}
+				catch (JsonReaderException) { }
+				catch (JsonSerializationException) { }
+				catch (JsonWriterException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/Newtonsoft.Json/Newtonsoft.Json.Run/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace Newtonsoft.Json.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			JsonConvert.DeserializeObject(text);
+		}
+	}
+}
