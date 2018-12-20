e7ce02ed6f23f06a7714a3c784315b57e5f2f1e5
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-20 11:20:12 +0100

    Add LumenWorksCsvReader

---
+++ src/SharpFuzz.Samples/LumenWorksCsvReader/LumenWorksCsvReader.Fuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using LumenWorks.Framework.IO.Csv;
+using SharpFuzz;
+
+namespace LumenWorksCsvReader.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenText(args[0]))
+					using (var csv = new CsvReader(file, false))
+					{
+						while (csv.ReadNextRecord()) { }
+					}
+				}
+				catch (IndexOutOfRangeException) { }
+				catch (MalformedCsvException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/LumenWorksCsvReader/LumenWorksCsvReader.Run/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using LumenWorks.Framework.IO.Csv;
+
+namespace LumenWorksCsvReader.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenText("/Users/Metalnem/Temp/findings/crashes/id:000000,sig:02,src:000000,op:havoc,rep:64"))
+			using (var csv = new CsvReader(file, false))
+			{
+				while (csv.ReadNextRecord()) { }
+			}
+		}
+	}
+}
