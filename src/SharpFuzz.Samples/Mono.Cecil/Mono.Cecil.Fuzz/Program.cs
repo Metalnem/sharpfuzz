0dee6e4e85b795ed0c74b44afa0c9943b681b35b
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-20 13:34:04 +0100

    Add Mono.Cecil

---
+++ src/SharpFuzz.Samples/Mono.Cecil/Mono.Cecil.Fuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SharpFuzz;
+
+namespace Mono.Cecil.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					ModuleDefinition.ReadModule(args[0], new ReaderParameters(ReadingMode.Immediate));
+				}
+				catch (AssemblyResolutionException) { }
+				catch (ArgumentOutOfRangeException) { }
+				catch (ArgumentException) { }
+				catch (BadImageFormatException) { }
+				catch (IndexOutOfRangeException) { }
+				catch (NotImplementedException) { }
+				catch (NotSupportedException) { }
+				catch (NullReferenceException) { }
+				catch (OverflowException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/Mono.Cecil/Mono.Cecil.Run/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mono.Cecil.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			ModuleDefinition.ReadModule(args[0], new ReaderParameters(ReadingMode.Immediate));
+		}
+	}
+}
