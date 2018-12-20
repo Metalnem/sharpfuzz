8746d118326004eec6e67dd658c223ce9c1ce352
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-20 14:48:04 +0100

    Add Utf8Json

---
+++ src/SharpFuzz.Samples/Utf8Json/Utf8Json.Fuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using SharpFuzz;
+
+namespace Utf8Json.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenRead(args[0]))
+					{
+						JsonSerializer.Deserialize<dynamic>(file);
+					}
+				}
+				catch (ArgumentNullException) { }
+				catch (ArgumentException) { }
+				catch (IndexOutOfRangeException) { }
+				catch (InvalidOperationException) { }
+				catch (JsonParsingException) { }
+			});
+		}
+	}
+}
