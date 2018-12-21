3a8aa4d66abb241d4a5cdb67eaca95e10313b612
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-21 10:48:18 +0100

    Add ExCSS

---
+++ src/SharpFuzz.Samples/ExCSS/ExCSS.Fuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using SharpFuzz;
+
+namespace ExCSS.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenRead(args[0]))
+					{
+						var parser = new StylesheetParser();
+						parser.Parse(file);
+					}
+				}
+				catch (ArgumentOutOfRangeException) { }
+				catch (ParseException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/ExCSS/ExCSS.Run/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace ExCSS.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenRead(args[0]))
+			{
+				var parser = new StylesheetParser();
+				parser.Parse(file);
+			}
+		}
+	}
+}
