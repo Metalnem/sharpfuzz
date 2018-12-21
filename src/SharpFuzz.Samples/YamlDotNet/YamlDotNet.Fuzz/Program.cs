a725c04e303fa0d683bd487e3f657a9ba37b7483
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-21 12:12:35 +0100

    Add YamlDotNet

---
+++ src/SharpFuzz.Samples/YamlDotNet/YamlDotNet.Fuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using SharpFuzz;
+using YamlDotNet.Core;
+using YamlDotNet.RepresentationModel;
+
+namespace YamlDotNet.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenText(args[0]))
+					{
+						new YamlStream().Load(file);
+					}
+				}
+				catch (ArgumentException) { }
+				catch (YamlException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/YamlDotNet/YamlDotNet.Run/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using YamlDotNet.RepresentationModel;
+
+namespace YamlDotNet.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenText(args[0]))
+			{
+				new YamlStream().Load(file);
+			}
+		}
+	}
+}
