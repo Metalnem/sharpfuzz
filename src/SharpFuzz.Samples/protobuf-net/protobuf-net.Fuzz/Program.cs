14dae0d5c661a6254d5b9802a058f85dfb1b0a00
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-21 13:36:00 +0100

    Add protobuf-net

---
+++ src/SharpFuzz.Samples/protobuf-net/protobuf-net.Fuzz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+using SharpFuzz;
+
+namespace protobuf_net.Fuzz
+{
+	[ProtoContract]
+	public class Person
+	{
+		[ProtoMember(1)] public int Id { get; set; }
+		[ProtoMember(2)] public string Name { get; set; }
+		[ProtoMember(3)] public Address Address { get; set; }
+	}
+
+	[ProtoContract]
+	public class Address
+	{
+		[ProtoMember(1)] public string Line1 { get; set; }
+		[ProtoMember(2)] public string Line2 { get; set; }
+	}
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenRead(args[0]))
+					{
+						Serializer.Deserialize<Person>(file);
+					}
+				}
+				catch (ArgumentException) { }
+				catch (EndOfStreamException) { }
+				catch (OverflowException) { }
+				catch (ProtoException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/protobuf-net/protobuf-net.Run/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using ProtoBuf;
+
+namespace protobuf_net.Run
+{
+	[ProtoContract]
+	public class Person
+	{
+		[ProtoMember(1)] public int Id { get; set; }
+		[ProtoMember(2)] public string Name { get; set; }
+		[ProtoMember(3)] public Address Address { get; set; }
+	}
+
+	[ProtoContract]
+	public class Address
+	{
+		[ProtoMember(1)] public string Line1 { get; set; }
+		[ProtoMember(2)] public string Line2 { get; set; }
+	}
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenRead(args[0]))
+			{
+				Serializer.Deserialize<Person>(file);
+			}
+		}
+	}
+}
