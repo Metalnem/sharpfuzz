1884cd96b627de90c96f28acff2a15c97002d58e
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-21 08:54:46 +0100

    Add SixLabors.Fonts

---
+++ src/SharpFuzz.Samples/SixLabors.Fonts/SixLabors.Fonts.Fuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using SharpFuzz;
+using SixLabors.Fonts.Exceptions;
+
+namespace SixLabors.Fonts.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					FontDescription.LoadDescription(args[0]);
+				}
+				catch (ArgumentException) { }
+				catch (EndOfStreamException) { }
+				catch (InvalidFontFileException) { }
+				catch (NullReferenceException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/SixLabors.Fonts/SixLabors.Fonts.Run/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SixLabors.Fonts.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			FontDescription.LoadDescription(args[0]);
+		}
+	}
+}
