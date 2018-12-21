268f650ea146407b198ad4a566685d9e5b49d1d6
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-21 09:37:19 +0100

    Add itext7

---
+++ src/SharpFuzz.Samples/itext7/itext7.Fuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using iText.IO;
+using iText.Kernel;
+using iText.Kernel.Pdf;
+using SharpFuzz;
+
+namespace itext7.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					new PdfDocument(new PdfReader(args[0]));
+				}
+				catch (FormatException) { }
+				catch (InvalidCastException) { }
+				catch (IOException) { }
+				catch (NullReferenceException) { }
+				catch (PdfException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/itext7/itext7.Run/Program.cs
@@ -0,0 +1,12 @@
+﻿using iText.Kernel.Pdf;
+
+namespace itext7.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			new PdfDocument(new PdfReader(args[0]));
+		}
+	}
+}
