d4b79a8cfc3a437d6fe79d6e05e2bcc407c5dcd5
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-21 08:34:45 +0100

    Add SixLabors.ImageSharp

---
+++ src/SharpFuzz.Samples/SixLabors.ImageSharp/SixLabors.ImageSharp.Fuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SharpFuzz;
+
+namespace SixLabors.ImageSharp.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					Image.Load(args[0]);
+				}
+				catch (ImageFormatException) { }
+				catch (NullReferenceException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/SixLabors.ImageSharp/SixLabors.ImageSharp.Run/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SixLabors.ImageSharp.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Image.Load(args[0]);
+		}
+	}
+}
