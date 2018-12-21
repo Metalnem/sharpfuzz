90bbc0bcb5dcabba5ce791c9bfd3732c53ccc50e
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-21 12:50:24 +0100

    Add NUglify

---
+++ src/SharpFuzz.Samples/NUglify/NUglify.Fuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using SharpFuzz;
+
+namespace NUglify.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				var text = File.ReadAllText(args[0]);
+				Uglify.Css(text);
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/NUglify/NUglify.Run/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace NUglify.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			Uglify.Css(text);
+		}
+	}
+}
