9b2940335fc736b5e3cac2ccc237774559c05585
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-21 10:52:00 +0100

    Add GraphQL-Parser

---
+++ src/SharpFuzz.Samples/GraphQL-Parser/GraphQL-Parser.Fuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using GraphQLParser;
+using GraphQLParser.Exceptions;
+using SharpFuzz;
+
+namespace GraphQL_Parser.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					var text = File.ReadAllText(args[0]);
+					var parser = new Parser(new Lexer());
+					parser.Parse(new Source(text));
+				}
+				catch (ArgumentOutOfRangeException) { }
+				catch (GraphQLSyntaxErrorException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/GraphQL-Parser/GraphQL-Parser.Run/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using GraphQLParser;
+
+namespace GraphQL_Parser.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			var parser = new Parser(new Lexer());
+			parser.Parse(new Source(text));
+		}
+	}
+}
