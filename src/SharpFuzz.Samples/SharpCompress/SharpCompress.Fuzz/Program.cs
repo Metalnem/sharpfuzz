7946460a8a678050abaed2e0993efad12357daa2
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-22 21:30:23 +0100

    Add SharpCompress

---
+++ src/SharpFuzz.Samples/SharpCompress/SharpCompress.Fuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+using SharpFuzz;
+
+namespace SharpCompress.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var zip = ZipArchive.Open(args[0]))
+					{
+						zip.Entries.ToList();
+					}
+				}
+				catch (ArchiveException) { }
+				catch (ArgumentException) { }
+				catch (CryptographicException) { }
+				catch (IOException) { }
+				catch (NotSupportedException) { }
+				catch (NullReferenceException) { }
+				catch (OutOfMemoryException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/SharpCompress/SharpCompress.Run/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using SharpCompress.Archives.Zip;
+
+namespace SharpCompress.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var zip = ZipArchive.Open(args[0]))
+			{
+				zip.Entries.ToList();
+			}
+		}
+	}
+}
