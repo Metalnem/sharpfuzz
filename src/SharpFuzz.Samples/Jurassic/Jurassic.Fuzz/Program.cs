76f8869f0e7f43957cbca6f7d047430ac1cfa777
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-22 14:12:16 +0100

    Add Jurassic

---
+++ src/SharpFuzz.Samples/Jurassic/Jurassic.Fuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SharpFuzz;
+
+namespace Jurassic.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					var engine = new ScriptEngine();
+					engine.ExecuteFile(args[0]);
+				}
+				catch (FormatException) { }
+				catch (JavaScriptException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/Jurassic/Jurassic.Run/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Jurassic.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var engine = new ScriptEngine();
+			engine.ExecuteFile(args[0]);
+		}
+	}
+}
