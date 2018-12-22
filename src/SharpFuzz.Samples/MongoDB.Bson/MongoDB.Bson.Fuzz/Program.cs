5c130d22935711fe9edd01a2d36179e71e7ec18c
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-22 19:26:35 +0100

    Add MongoDB.Bson

---
+++ src/SharpFuzz.Samples/MongoDB.Bson/MongoDB.Bson.Fuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using SharpFuzz;
+
+namespace MongoDB.Bson.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenRead(args[0]))
+					using (var bson = new BsonBinaryReader(file))
+					{
+						BsonSerializer.Deserialize(bson, typeof(object));
+					}
+				}
+				catch (ArgumentOutOfRangeException) { }
+				catch (DecoderFallbackException) { }
+				catch (ArgumentException) { }
+				catch (FormatException) { }
+				catch (IndexOutOfRangeException) { }
+				catch (IOException) { }
+				catch (OutOfMemoryException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/MongoDB.Bson/MongoDB.Bson.Run/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+
+namespace MongoDB.Bson.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenRead(args[0]))
+			using (var bson = new BsonBinaryReader(file))
+			{
+				BsonSerializer.Deserialize(bson, typeof(object));
+			}
+		}
+	}
+}
