328e0faafa76997aa6af6b33e2202cc7cbade117
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-22 10:44:26 +0100

    Add some documentation

--- src/SharpFuzz/Fault.cs
+++ src/SharpFuzz/Fault.cs
@@ -1,5 +1,7 @@
 namespace SharpFuzz
 {
+	// afl-fuzz execution status fault codes (only
+	// success and crash are currently being used).
 	internal enum Fault : byte
 	{
 		None = 0,
--- src/SharpFuzz/SharedMemoryHandle.cs
+++ src/SharpFuzz/SharedMemoryHandle.cs
@@ -3,6 +3,9 @@ using Microsoft.Win32.SafeHandles;
 
 namespace SharpFuzz
 {
+	// Represents a wrapper class for a shared memory handle,
+	// obtained by attaching the shared memory segment to the address
+	// space of the calling process using the shmat system call.
 	internal sealed class SharedMemoryHandle : SafeHandleMinusOneIsInvalid
 	{
 		public SharedMemoryHandle() : base(true)
