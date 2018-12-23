092be2d8c8e3bef59c4797639dc47460a82fa378
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-23 21:34:40 +0100

    Add MarkdownSharp

---
+++ src/SharpFuzz.Samples/MarkdownSharp/MarkdownSharp.Fuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using SharpFuzz;
+
+namespace MarkdownSharp.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				var text = File.ReadAllText(args[0]);
+				new Markdown().Transform(text);
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/MarkdownSharp/MarkdownSharp.Run/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace MarkdownSharp.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			new Markdown().Transform(text);
+		}
+	}
+}
