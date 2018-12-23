fe35263c09c65abf23bf5dac97fff83ba7359fc1
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-23 17:59:56 +0100

    Add Markdig

---
+++ src/SharpFuzz.Samples/Markdig/Markdig.Fuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using SharpFuzz;
+
+namespace Markdig.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					var text = File.ReadAllText(args[0]);
+					var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+					Markdown.ToHtml(text, pipeline);
+				}
+				catch (IndexOutOfRangeException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/Markdig/Markdig.Run/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Markdig.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+			Markdown.ToHtml(text, pipeline);
+		}
+	}
+}
