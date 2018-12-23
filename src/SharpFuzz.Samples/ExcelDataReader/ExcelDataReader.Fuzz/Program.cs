1fae3a36e02976748dc6ea31e40e0c6baa49cefa
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-23 09:40:09 +0100

    Add ExcelDataReader

---
+++ src/SharpFuzz.Samples/ExcelDataReader/ExcelDataReader.Fuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using ExcelDataReader.Exceptions;
+using SharpFuzz;
+
+namespace ExcelDataReader.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenRead(args[0]))
+					using (var reader = ExcelReaderFactory.CreateBinaryReader(file))
+					{
+						do
+						{
+							while (reader.Read()) { }
+						} while (reader.NextResult());
+					}
+				}
+				catch (ArgumentOutOfRangeException) { }
+				catch (ExcelReaderException) { }
+				catch (FormatException) { }
+				catch (InvalidOperationException) { }
+				catch (OutOfMemoryException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/ExcelDataReader/ExcelDataReader.Run/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ExcelDataReader.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+			using (var file = File.OpenRead(args[0]))
+			using (var reader = ExcelReaderFactory.CreateBinaryReader(file))
+			{
+				do
+				{
+					while (reader.Read()) { }
+				} while (reader.NextResult());
+			}
+		}
+	}
+}
