5d855f9a51158756ed7dfda5208196c7c3ec1914
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-23 09:05:50 +0100

    Add WebMarkupMin.Core

---
+++ src/SharpFuzz.Samples/WebMarkupMin.Core/WebMarkupMin.Core.Fuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using SharpFuzz;
+
+namespace WebMarkupMin.Core.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				var text = File.ReadAllText(args[0]);
+				new HtmlMinifier().Minify(text);
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/WebMarkupMin.Core/WebMarkupMin.Core.Run/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace WebMarkupMin.Core.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			new HtmlMinifier().Minify(text);
+		}
+	}
+}
