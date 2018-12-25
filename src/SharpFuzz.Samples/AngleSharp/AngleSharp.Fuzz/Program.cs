7976b29b808ba37f6bc29c04885ad3fe8ad0031b
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-25 09:06:14 +0100

    Add AngleSharp

---
+++ src/SharpFuzz.Samples/AngleSharp/AngleSharp.Fuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using AngleSharp.Parser.Html;
+using SharpFuzz;
+
+namespace AngleSharp.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				using (var file = File.OpenRead(args[0]))
+				{
+					new HtmlParser().Parse(file);
+				}
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/AngleSharp/AngleSharp.Run/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using AngleSharp.Parser.Html;
+
+namespace AngleSharp.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenRead(args[0]))
+			{
+				new HtmlParser().Parse(file);
+			}
+		}
+	}
+}
