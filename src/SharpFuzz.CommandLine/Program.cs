c705b7f99f6d89fa5d8d72bb652e62c9abdcfb54
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-25 11:46:05 +0100

    Add sharpfuzz command line instrumentation tool

---
+++ src/SharpFuzz.CommandLine/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace SharpFuzz.CommandLine
+{
+	public class Program
+	{
+		private const string Usage = @"Usage: sharpfuzz [path-to-assembly]
+
+path-to-assembly:
+  The path to an assembly .dll file to instrument.";
+
+		public static void Main(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine(Usage);
+				return;
+			}
+
+			string path = args[0];
+
+			if (!File.Exists(path))
+			{
+				Console.Error.WriteLine("Specified file does not exist.");
+				return;
+			}
+
+			try
+			{
+				Fuzzer.Instrument(path);
+			}
+			catch
+			{
+				Console.Error.WriteLine("Specified file is not a valid .NET assembly.");
+				return;
+			}
+		}
+	}
+}
