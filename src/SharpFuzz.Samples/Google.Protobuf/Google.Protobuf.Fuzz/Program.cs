82e681c4aa5f061bf282f11e92a2b68de516c045
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-26 12:17:56 +0100

    Add Google.Protobuf

---
+++ src/SharpFuzz.Samples/Google.Protobuf/Google.Protobuf.Fuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Sample;
+using SharpFuzz;
+
+namespace Google.Protobuf.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					using (var file = File.OpenRead(args[0]))
+					{
+						Person.Parser.ParseFrom(file);
+					}
+				}
+				catch (ArgumentOutOfRangeException) { }
+				catch (InvalidOperationException) { }
+				catch (InvalidProtocolBufferException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/Google.Protobuf/Google.Protobuf.Run/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Sample;
+
+namespace Google.Protobuf.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var file = File.OpenRead(args[0]))
+			{
+				Person.Parser.ParseFrom(file);
+			}
+		}
+	}
+}
