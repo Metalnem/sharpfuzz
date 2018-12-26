27ab3791fbd5d57efea5d529f2775acb7e3b2d0a
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-26 09:27:44 +0100

    Add NCrontab

---
+++ src/SharpFuzz.Samples/NCrontab/NCrontab.Fuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using SharpFuzz;
+
+namespace NCrontab.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					var text = File.ReadAllText(args[0]);
+					CrontabSchedule.Parse(text);
+				}
+				catch (CrontabException) { }
+				catch (OverflowException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/NCrontab/NCrontab.Run/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace NCrontab.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			CrontabSchedule.Parse(text);
+		}
+	}
+}
