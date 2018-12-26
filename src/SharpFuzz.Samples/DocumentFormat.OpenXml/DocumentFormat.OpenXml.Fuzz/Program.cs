188674690473b7b6e57018f9706274f468049574
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-26 15:35:15 +0100

    Add DocumentFormat.OpenXml

---
+++ src/SharpFuzz.Samples/DocumentFormat.OpenXml/DocumentFormat.OpenXml.Fuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using DocumentFormat.OpenXml.Packaging;
+using SharpFuzz;
+
+namespace DocumentFormat.OpenXml.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					SpreadsheetDocument.Open(args[0], false);
+				}
+				catch (ArgumentException) { }
+				catch (FileFormatException) { }
+				catch (InvalidDataException) { }
+				catch (InvalidOperationException) { }
+				catch (IOException) { }
+				catch (OpenXmlPackageException) { }
+				catch (XmlException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/DocumentFormat.OpenXml/DocumentFormat.OpenXml.Run/Program.cs
@@ -0,0 +1,12 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+
+namespace DocumentFormat.OpenXml.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			SpreadsheetDocument.Open(args[0], false);
+		}
+	}
+}
