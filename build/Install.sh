b2e0cbd58959968a8e423af02b3c32b171635754
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-12-31 14:39:10 +0100

    Add the afl-fuzz and SharpFuzz.CommandLine install script

---
+++ build/Install.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+set -eux
+
+# Download and extract the latest afl-fuzz source package
+wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
+tar -xvf afl-latest.tgz
+
+rm afl-latest.tgz
+cd afl-2.52b/
+
+# Patch afl-fuzz so that it doesn't check whether the binary
+# being fuzzed is instrumented (we have to do this because
+# we are going to run our programs with the dotnet run command,
+# and the dotnet binary would fail this check)
+wget https://github.com/Metalnem/sharpfuzz/raw/master/patches/RemoveInstrumentationCheck.diff
+patch < RemoveInstrumentationCheck.diff
+
+# Install afl-fuzz
+make install
+rm -rf afl-2.52b/
+
+# Install SharpFuzz.CommandLine global .NET tool
+dotnet tool install --global SharpFuzz.CommandLine --version 0.7.0
