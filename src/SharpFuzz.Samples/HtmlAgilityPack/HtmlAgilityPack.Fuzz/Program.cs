25ed5760825b15254abcb6e3b002f4a20b52d531
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-01-02 14:44:39 +0100

    Add HtmlAgilityPack

---
+++ src/SharpFuzz.Samples/HtmlAgilityPack/HtmlAgilityPack.Fuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿using SharpFuzz;
+
+namespace HtmlAgilityPack.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				new HtmlDocument().Load(args[0]);
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/HtmlAgilityPack/HtmlAgilityPack.Run/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HtmlAgilityPack.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			new HtmlDocument().Load(args[0]);
+		}
+	}
+}
