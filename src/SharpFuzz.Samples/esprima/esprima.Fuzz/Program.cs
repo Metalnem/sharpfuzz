a76f01660ad70afa6721ae4b0a86ef7466bba3d1
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-01-04 11:09:17 +0100

    Fuzz esprima

---
+++ src/SharpFuzz.Samples/esprima/esprima.Fuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Esprima;
+using SharpFuzz;
+
+namespace esprima.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				try
+				{
+					var text = File.ReadAllText(args[0]);
+					var parser = new JavaScriptParser(text);
+					parser.ParseProgram();
+				}
+				catch (IndexOutOfRangeException) { }
+				catch (InvalidCastException) { }
+				catch (InvalidOperationException) { }
+				catch (OverflowException) { }
+				catch (ParserException) { }
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/esprima/esprima.Run/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Esprima;
+
+namespace esprima.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			var parser = new JavaScriptParser(text);
+			parser.ParseProgram();
+		}
+	}
+}
