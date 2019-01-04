1576805754e9cc61fc3a3f7e2f1b3ebbad3e12c4
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-01-04 10:11:12 +0100

    Fuzz Iced

---
+++ src/SharpFuzz.Samples/Iced/Iced.Fuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Iced.Intel;
+using SharpFuzz;
+
+namespace Iced.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				var code = File.ReadAllBytes(args[0]);
+				var reader = new ByteArrayCodeReader(code);
+
+				var decoder = Decoder.Create(64, reader);
+				decoder.InstructionPointer = 0x00007FFAC46ACDA4;
+
+				var end = decoder.InstructionPointer + (uint)code.Length;
+				var instructions = new InstructionList();
+
+				while (decoder.InstructionPointer < end)
+				{
+					decoder.Decode(out instructions.AllocUninitializedElement());
+				}
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/Iced/Iced.Run/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Iced.Intel;
+
+namespace Iced.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var code = File.ReadAllBytes(args[0]);
+			var reader = new ByteArrayCodeReader(code);
+
+			var decoder = Decoder.Create(64, reader);
+			decoder.InstructionPointer = 0x00007FFAC46ACDA4;
+
+			var end = decoder.InstructionPointer + (uint)code.Length;
+			var instructions = new InstructionList();
+
+			while (decoder.InstructionPointer < end)
+			{
+				decoder.Decode(out instructions.AllocUninitializedElement());
+			}
+		}
+	}
+}
