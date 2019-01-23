aa9540e130c6e24ab87baff6da22a5db367224f4
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-01-23 10:49:41 +0100

    Also update Install.sh script

--- build/Install.sh
+++ build/Install.sh
@@ -20,4 +20,4 @@ make install
 rm -rf ../afl-2.52b/
 
 # Install SharpFuzz.CommandLine global .NET tool
-dotnet tool install --global SharpFuzz.CommandLine --version 0.7.0
+dotnet tool install --global SharpFuzz.CommandLine --version 1.1.0
