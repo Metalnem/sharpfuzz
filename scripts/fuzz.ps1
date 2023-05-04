2accb56dfe9b601ea5d707ed1662013761c3aa42
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-05-04 15:41:51 +0200

    Update the fuzzing script

--- scripts/fuzz.ps1
+++ scripts/fuzz.ps1
@@ -1,13 +1,23 @@
 param (
     [Parameter(Mandatory = $true)]
     [string]$projectPath,
-    [Parameter(Mandatory = $true)]
-    [string[]]$fuzzingTargets,
     [string]$testcasesDir = "testcases",
     [string]$findingsDir = "findings"
 )
 
 Set-StrictMode -Version Latest
+
+if (!(Test-Path $testcasesDir)) {
+    Write-Error "Testcases directory $testcasesDir does not exist"
+    exit 1
+}
+
+$testcases = Get-ChildItem $testcasesDir
+
+if (!$testcases) {
+    Write-Error "Testcases directory $testcasesDir is empty"
+    exit 1
+}
 
 $outputDir = "bin"
 
@@ -16,18 +26,29 @@ if (Test-Path $findingsDir) { Remove-Item -Recurse -Force $findingsDir }
 
 dotnet publish $projectPath -c release -o $outputDir
 
+$projectName = (Get-Item $projectPath).BaseName
+$projectDll = "$projectName.dll"
+$projectPath = Join-Path $outputDir $projectDll
+
+$exclusions = @(
+    "dnlib.dll",
+    "SharpFuzz.dll",
+    "SharpFuzz.Common.dll",
+    $projectDll
+)
+
+$fuzzingTargets = Get-ChildItem $outputDir -Filter *.dll `
+| Where-Object { $_.Name -notin $exclusions } `
+| Where-Object { $_.Name -notlike "System.*.dll" }
+
 foreach ($fuzzingTarget in $fuzzingTargets) {
-    $fuzzingTargetPath = Join-Path $outputDir $fuzzingTarget
-    Write-Output "Instrumenting $($fuzzingTargetPath)"
-    sharpfuzz $fuzzingTargetPath
+    Write-Output "Instrumenting $fuzzingTarget"
+    sharpfuzz $fuzzingTarget
     
     if ($LastExitCode -ne 0) {
-        Write-Error "An error occurred while instrumenting $($fuzzingTargetPath)"
+        Write-Error "An error occurred while instrumenting $fuzzingTarget"
         exit 1
     }
 }
-
-$projectName = (Get-Item $projectPath).BaseName
-$projectPath = Join-Path $outputDir "$projectName.dll"
 
 afl-fuzz -i $testcasesDir -o $findingsDir -t 10000 -m 10000 dotnet $projectPath
