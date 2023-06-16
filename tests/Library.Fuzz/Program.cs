682518dd91130b3cbf0bf8667abd61e1fb0aa3bc
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-16 19:26:00 +0200

    Initial version

---
+++ tests/Library.Fuzz/Program.cs
@@ -0,0 +1,14 @@
+﻿using SharpFuzz;
+
+namespace Library.Fuzz;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Fuzzer.Run(s =>
+        {
+            Parser.Parse(s);
+        });
+    }
+}
---
+++ tests/Library/Parser.cs
@@ -0,0 +1,23 @@
+﻿namespace Library;
+
+public static class Parser
+{
+    public static void Parse(string s)
+    {
+        if (s.Length > 0 && s[0] == 'C')
+        if (s.Length > 1 && s[1] == 'o')
+        if (s.Length > 2 && s[2] == 'o')
+        if (s.Length > 3 && s[3] == 'k')
+        if (s.Length > 4 && s[4] == 'i')
+        if (s.Length > 5 && s[5] == 'n')
+        if (s.Length > 6 && s[6] == 'g')
+        if (s.Length > 7 && s[7] == ' ')
+        if (s.Length > 8 && s[8] == 'M')
+        if (s.Length > 9 && s[9] == 'C')
+        if (s.Length > 10 && s[10] == '\'')
+        if (s.Length > 11 && s[11] == 's')
+        {
+            throw new Exception("Like a pound of bacon");
+        }
+    }
+}
