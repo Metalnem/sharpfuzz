f7703e460c314daf89c409084e18758a0be8e9ca
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-17 15:41:56 +0200

    Use SharpFuzz.CommandLine

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,5 +1,6 @@
 New-Item -Path "corpus/test" -ItemType File -Force -Value "W"
-& scripts/fuzz.ps1 tests/Library.Fuzz/Library.Fuzz.csproj -i corpus
+dotnet publish src/SharpFuzz.CommandLine/SharpFuzz.CommandLine.csproj -o out -c release -f net8.0
+& scripts/fuzz.ps1 tests/Library.Fuzz/Library.Fuzz.csproj -i corpus -command out/SharpFuzz.CommandLine
 
 $output = Get-Content -Path "./findings/.cur_input" -Raw
 $crasher = "Whoopsie"
