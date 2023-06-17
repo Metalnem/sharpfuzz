c04c73eb931b382708a251f60714d98b2b79154f
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-17 12:35:48 +0200

    run tests

--- tests/Library/Parser.cs
+++ tests/Library/Parser.cs
@@ -4,20 +4,16 @@ public static class Parser
 {
     public static void Parse(string s)
     {
-        if (s.Length > 0 && s[0] == 'C')
-        if (s.Length > 1 && s[1] == 'o')
+        if (s.Length > 0 && s[0] == 'W')
+        if (s.Length > 1 && s[1] == 'h')
         if (s.Length > 2 && s[2] == 'o')
-        if (s.Length > 3 && s[3] == 'k')
-        if (s.Length > 4 && s[4] == 'i')
-        if (s.Length > 5 && s[5] == 'n')
-        if (s.Length > 6 && s[6] == 'g')
-        if (s.Length > 7 && s[7] == ' ')
-        if (s.Length > 8 && s[8] == 'M')
-        if (s.Length > 9 && s[9] == 'C')
-        if (s.Length > 10 && s[10] == '\'')
-        if (s.Length > 11 && s[11] == 's')
+        if (s.Length > 3 && s[3] == 'o')
+        if (s.Length > 4 && s[4] == 'p')
+        if (s.Length > 5 && s[5] == 's')
+        if (s.Length > 6 && s[6] == 'i')
+        if (s.Length > 7 && s[7] == 'e')
         {
-            Environment.FailFast("Like a pound of bacon");
+            Environment.FailFast("Everything is on fire");
         }
     }
 }
