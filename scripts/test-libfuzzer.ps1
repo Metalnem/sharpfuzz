e90fed0e63bdb3903d6fed7bef467d796599c0dd
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-18 16:44:46 +0200

    libFuzzer support

---
+++ scripts/test-libfuzzer.ps1
@@ -0,0 +1,21 @@
+$libFuzzer = libfuzzer-dotnet-windows.exe
+$uri = "https://github.com/metalnem/libfuzzer-dotnet/releases/latest/download/$libFuzzer"
+
+Invoke-WebRequest -Uri $uri -OutFile $file
+New-Item -Path "corpus" -ItemType Directory
+
+dotnet publish src/SharpFuzz.CommandLine/SharpFuzz.CommandLine.csproj `
+    --output out `
+    --configuration release `
+    --framework net8.0
+
+& scripts/fuzz-libfuzzer.ps1 `
+    -libFuzzer $libFuzzer `
+    -project tests/Library.LibFuzzer/Library.LibFuzzer.csproj `
+    -corpus "corpus" `
+    -command out/SharpFuzz.CommandLine
+
+$crasher = "Whoopsie"
+
+Write-Host $crasher
+exit 0
